43f0498be8964a49d8576b24a299a75b57002b83
Lucas <87961444+nozomichi00@users.noreply.github.com>
2024-10-10 09:05:21 -0700

    Ver.2 Update Done.
    1. 追加檔案切割。
    2. 調整介面排版。
    3. 追加字串切行長度定義。
    4. 追加檔案切割大小自定義。

--- LogTypeConvertTool Ver.2.py
+++ LogTypeConvertTool Ver.2.py
@@ -3,10 +3,6 @@ import os, re, random, base64, textwrap
 from datetime import datetime, timedelta
 from tkinter import filedialog, messagebox
 
-# customtkinter擴展
-from CTkToolTip import *
-from CTkMessagebox import CTkMessagebox
-
 # 設定外觀與主題
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("blue")
@@ -20,7 +16,7 @@ def select_input_file():
 # 選擇輸出資料夾
 def select_output_folder():
     selected_folder = filedialog.askdirectory()
-    if selected_folder:  # 檢查是否選擇了資料夾
+    if selected_folder:
         output_folder.set(selected_folder)
 
 # 產生時間選項
@@ -29,25 +25,33 @@ def generate_time_options():
     options = []
     for day_offset in range(-3, 1):
         current_day = now + timedelta(days=day_offset)
-        for hour in range(0, 24, 3):  # 每3小時一個選項
+        for hour in range(0, 24, 3):
             time_option = current_day.replace(hour=hour, minute=0).strftime('%Y/%m/%d %H:00')
             options.append(time_option)
     return options
 
 # 轉換檔案
 def convert_file():
-    selected_files = input_path.get().split(",")  # 取得多個檔案的清單
+    result_label.configure(text="Converting file, please wait...")
+    root.update_idletasks()
+
+    # 取得多個檔案的清單
+    selected_files = input_path.get().split(",")
     if len(selected_files) > 1:
         messagebox.showerror("Error", "Please select only one file for conversion.")
         return
     
-    if not input_path.get():  # 檢查是否選擇了檔案
+    # 檢查是否選擇了檔案
+    if not input_path.get():
         messagebox.showerror("Error", "Please select a compressed file.")
         return
-    if not output_folder.get():  # 檢查是否選擇了輸出資料夾
+    
+    # 檢查是否選擇了輸出資料夾
+    if not output_folder.get():
         messagebox.showerror("Error", "Please select an output folder.")
         return
 
+    # 檢查時間區間是否有效
     start_time = datetime.strptime(start_time_combobox.get(), '%Y/%m/%d %H:%M')
     end_time = datetime.strptime(end_time_combobox.get(), '%Y/%m/%d %H:%M')
     if start_time >= end_time:
@@ -115,23 +119,29 @@ def convert_file():
 
 # 還原檔案
 def restore_file():
-    selected_files = input_path.get().split("\n")  # 用换行符分割檔案清單
+    result_label.configure(text="Restoring file, please wait...")
+    root.update_idletasks()
+
+    # 用换行符分割檔案清單
+    selected_files = input_path.get().split("\n")
     if not all(file.endswith(".txt") for file in selected_files):
         result_label.configure(text="Please select valid .txt files for restoration.")
         return
 
+    # 將每個檔案的內容添加到數據中
     combined_data = []
     for file in selected_files:
         try:
             with open(file, "r") as f:
-                combined_data.extend(f.readlines())  # 將每個檔案的內容添加到數據中
+                combined_data.extend(f.readlines())
         except Exception as e:
             messagebox.showerror("Error", f"Error reading file: {str(e)}")
             return
 
+    # 移除時間戳
     clean_data = []
     for line in combined_data:
-        clean_line = re.sub(r'^\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2}\.\d{3}\s+', '', line)  # 移除時間戳
+        clean_line = re.sub(r'^\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2}\.\d{3}\s+', '', line)
         clean_data.append(clean_line.strip())
 
     # 合併所有乾淨數據為單一字符串
@@ -151,7 +161,6 @@ def restore_file():
         messagebox.showerror("Error", f"Failed to decode data: {str(e)}")
         return
 
-    # 儲存還原後的檔案
     output_path = os.path.join(output_folder.get(), f"{output_file_name.get()}.zip")
     with open(output_path, "wb") as f:
         f.write(byte_data)
@@ -160,12 +169,45 @@ def restore_file():
 
 # 顯示關於對話框
 def show_about():
-    messagebox.showinfo("About", "This is a file conversion tool.")
+    messagebox.showinfo("About", 
+                        "This tool converts compressed files into strings. Please send the string via email,\n"
+                        "and then convert it back to the compressed file.\n"
+                        "It is recommended to use the Hexadecimal mode.\n\n"
+                        "Example of file compression ratio:\n"
+                        "1. Original Log: 174 MB\n"
+                        "2. Compressed Size: 9.24 MB\n"
+                        "3. Compressed String Sizes for Different Types:\n"
+                        "   Binary_output.txt: 104 MB\n"
+                        "   Decimal_output.txt: 41.0 MB\n"
+                        "   Hexadecimal_output.txt: 34.7 MB\n"
+                        "   Base64_output.txt: 15.5 MB\n"
+                        "4. Restored Size: 9.24 MB; Uncompressed Size: 174 MB\n\n"
+                        "===============================================\n\n"
+                        "此工具使用方式是將壓縮檔案轉換成字串，請客戶信件寄出後，\n"
+                        "再將字串轉換回壓縮檔。\n"
+                        "建議使用: Hexadecimal 模式\n\n"
+                        "以下是檔案壓縮率的範例：\n"
+                        "1. 原始Log共 174 MB\n"
+                        "2. 壓縮後共 9.24 MB\n"
+                        "3. 轉換字串後不同類型的壓縮：\n"
+                        "   Binary_output.txt: 104 MB\n"
+                        "   Decimal_output.txt: 41.0 MB\n"
+                        "   Hexadecimal_output.txt: 34.7 MB\n"
+                        "   Base64_output.txt: 15.5 MB\n"
+                        "4. 還原後共 9.24 MB，解壓縮後共 174 MB")
 
 # 建立主視窗
 root = ctk.CTk()
 root.title("File Conversion Tool Ver.2")
-root.geometry("550x500")
+root.geometry("570x550")
+
+# 創建可滾動的框架
+scrollable_frame = ctk.CTkScrollableFrame(root)
+scrollable_frame.grid(row=0, column=0, sticky="nsew")
+
+# 調整主視窗格局以允許滾動
+root.grid_rowconfigure(0, weight=1)
+root.grid_columnconfigure(0, weight=1)
 
 input_path = ctk.StringVar()
 output_folder = ctk.StringVar()
@@ -173,49 +215,49 @@ max_line_length = ctk.StringVar(value="100")
 max_file_size = ctk.StringVar(value="3000")
 output_file_name = ctk.StringVar(value="OutputFileName")
 
-# 建立 UI 元件
-ctk.CTkLabel(root, text="Select input file (zip/txt):").grid(row=0, column=0, padx=10, pady=10, sticky="e")
-ctk.CTkEntry(root, textvariable=input_path, width=200).grid(row=0, column=1, padx=10, pady=10, sticky="w")
-ctk.CTkButton(root, text="Browse", command=select_input_file).grid(row=0, column=2, padx=10, pady=10)
+# 在滾動框架內創建 UI 元件
+ctk.CTkLabel(scrollable_frame, text="Select input file (zip/txt):").grid(row=0, column=0, padx=10, pady=10, sticky="e")
+ctk.CTkEntry(scrollable_frame, textvariable=input_path, width=200).grid(row=0, column=1, padx=10, pady=10, sticky="w")
+ctk.CTkButton(scrollable_frame, text="Browse", command=select_input_file).grid(row=0, column=2, padx=10, pady=10)
 
-ctk.CTkLabel(root, text="Select output folder:").grid(row=1, column=0, padx=10, pady=10, sticky="e")
-ctk.CTkEntry(root, textvariable=output_folder, width=200).grid(row=1, column=1, padx=10, pady=10, sticky="w")
-ctk.CTkButton(root, text="Browse", command=select_output_folder).grid(row=1, column=2, padx=10, pady=10)
+ctk.CTkLabel(scrollable_frame, text="Select output folder:").grid(row=1, column=0, padx=10, pady=10, sticky="e")
+ctk.CTkEntry(scrollable_frame, textvariable=output_folder, width=200).grid(row=1, column=1, padx=10, pady=10, sticky="w")
+ctk.CTkButton(scrollable_frame, text="Browse", command=select_output_folder).grid(row=1, column=2, padx=10, pady=10)
 
-ctk.CTkLabel(root, text="Select start time:").grid(row=2, column=0, padx=10, pady=10, sticky="e")
-start_time_combobox = ctk.CTkComboBox(root, values=generate_time_options(), width=200)
+ctk.CTkLabel(scrollable_frame, text="Select start time:").grid(row=2, column=0, padx=10, pady=10, sticky="e")
+start_time_combobox = ctk.CTkComboBox(scrollable_frame, values=generate_time_options(), width=200)
 start_time_combobox.set(generate_time_options()[0])
 start_time_combobox.grid(row=2, column=1, padx=10, pady=10, sticky="w")
 
-ctk.CTkLabel(root, text="Select end time:").grid(row=3, column=0, padx=10, pady=10, sticky="e")
-end_time_combobox = ctk.CTkComboBox(root, values=generate_time_options(), width=200)
+ctk.CTkLabel(scrollable_frame, text="Select end time:").grid(row=3, column=0, padx=10, pady=10, sticky="e")
+end_time_combobox = ctk.CTkComboBox(scrollable_frame, values=generate_time_options(), width=200)
 end_time_combobox.set(generate_time_options()[-1])
 end_time_combobox.grid(row=3, column=1, padx=10, pady=10, sticky="w")
 
-ctk.CTkLabel(root, text="Max line length:").grid(row=4, column=0, padx=10, pady=10, sticky="e")
-ctk.CTkEntry(root, textvariable=max_line_length, width=200).grid(row=4, column=1, padx=10, pady=10, sticky="w")
+ctk.CTkLabel(scrollable_frame, text="Max line length:").grid(row=4, column=0, padx=10, pady=10, sticky="e")
+ctk.CTkEntry(scrollable_frame, textvariable=max_line_length, width=200).grid(row=4, column=1, padx=10, pady=10, sticky="w")
 
-ctk.CTkLabel(root, text="Max file size (KB):").grid(row=5, column=0, padx=10, pady=10, sticky="e")
-ctk.CTkEntry(root, textvariable=max_file_size, width=200).grid(row=5, column=1, padx=10, pady=10, sticky="w")
+ctk.CTkLabel(scrollable_frame, text="Max file size (KB):").grid(row=5, column=0, padx=10, pady=10, sticky="e")
+ctk.CTkEntry(scrollable_frame, textvariable=max_file_size, width=200).grid(row=5, column=1, padx=10, pady=10, sticky="w")
 
-ctk.CTkLabel(root, text="Conversion type:").grid(row=6, column=0, padx=10, pady=10, sticky="e")
-conversion_combobox = ctk.CTkComboBox(root, values=["Binary", "Decimal", "Hexadecimal", "Base64"], width=200)
+ctk.CTkLabel(scrollable_frame, text="Conversion type:").grid(row=6, column=0, padx=10, pady=10, sticky="e")
+conversion_combobox = ctk.CTkComboBox(scrollable_frame, values=["Binary", "Decimal", "Hexadecimal", "Base64"], width=200)
 conversion_combobox.set("Hexadecimal")
 conversion_combobox.grid(row=6, column=1, padx=10, pady=10, sticky="w")
 
-ctk.CTkLabel(root, text="Output file name:").grid(row=7, column=0, padx=10, pady=10, sticky="e")
-ctk.CTkEntry(root, textvariable=output_file_name, width=200).grid(row=7, column=1, padx=10, pady=10, sticky="w")
+ctk.CTkLabel(scrollable_frame, text="Output file name:").grid(row=7, column=0, padx=10, pady=10, sticky="e")
+ctk.CTkEntry(scrollable_frame, textvariable=output_file_name, width=200).grid(row=7, column=1, padx=10, pady=10, sticky="w")
 
-ctk.CTkButton(root, text="Convert", command=convert_file).grid(row=8, column=0, padx=10, pady=10)
-ctk.CTkButton(root, text="Restore", command=restore_file).grid(row=8, column=1, padx=10, pady=10)
+ctk.CTkButton(scrollable_frame, text="Convert", command=convert_file).grid(row=8, column=0, padx=10, pady=10)
+ctk.CTkButton(scrollable_frame, text="Restore", command=restore_file).grid(row=8, column=1, padx=10, pady=10)
 
-ctk.CTkButton(root, text="About", command=show_about).grid(row=8, column=2, padx=10, pady=10)
+ctk.CTkButton(scrollable_frame, text="About", command=show_about).grid(row=8, column=2, padx=10, pady=10)
 
-ctk.CTkLabel(root, text="Selected files:").grid(row=9, column=0, padx=10, pady=10, sticky="e")
-selected_files_label = ctk.CTkLabel(root, textvariable=input_path)
-selected_files_label.grid(row=9, column=1, padx=10, pady=10, sticky="w")
+result_label = ctk.CTkLabel(scrollable_frame, text="", text_color="cyan")
+result_label.grid(row=9, column=0, columnspan=3)
 
-result_label = ctk.CTkLabel(root, text="")
-result_label.grid(row=10, column=0, columnspan=3)
+ctk.CTkLabel(scrollable_frame, text="Selected files:").grid(row=10, column=0, padx=10, pady=(10, 0), sticky="W")
+selected_files_label = ctk.CTkLabel(scrollable_frame, textvariable=input_path)
+selected_files_label.grid(row=11, column=0, columnspan=2, padx=10, pady=(0, 10), sticky="w")
 
 root.mainloop()
